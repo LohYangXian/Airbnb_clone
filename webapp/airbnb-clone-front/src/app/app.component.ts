9af999d1a22bc41bbf3f11f46a21924eb53b1ab7
LohYangXian <lohyangxian@gmail.com>
2024-06-20 16:59:23 +0800

    Added navbar, footer, and toastservice

--- webapp/airbnb-clone-front/src/app/app.component.ts
+++ webapp/airbnb-clone-front/src/app/app.component.ts
@@ -3,23 +3,43 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ToastModule } from 'primeng/toast';
+import { ToastService } from './layout/toast.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, NavbarComponent, FooterComponent, ToastModule],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   
   faIconLibrary = inject(FaIconLibrary);
+  isListingView = true;
+  toastService = inject(ToastService);
+  messageService = inject(MessageService);
 
   ngOnInit(): void {
     this.initFontAwesome();
+    this.listenToToastService();
   }
 
   private initFontAwesome() {
     this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  private listenToToastService() {
+    this.toastService.sendSub.subscribe({
+      next: newMessage => {
+        if(newMessage && newMessage.summary !== this.toastService.INIT_STATE) {
+          this.messageService.add(newMessage);
+        }
+      }
+    })
   }
 }
---
+++ webapp/airbnb-clone-front/src/app/layout/navbar/category/category.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CategoryService } from './category.service';
+import { Category } from './category.model';
+
+@Component({
+  selector: 'app-category',
+  standalone: true,
+  imports: [
+    FontAwesomeModule
+  ],
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.scss'
+})
+export class CategoryComponent implements OnInit {
+  categoryService = inject(CategoryService);
+  categories: Category[] | undefined;
+  currentActivateCategory = this.categoryService.getCategoryByDefault();
+
+  ngOnInit(): void {
+    this.categories = this.categoryService.getCategories();
+  }
+}
