a9ef8cb10f08cfa9db5ddb8f7a43d25b47c348f1
LohYangXian <e0726433@u.nus.edu>
2024-06-21 07:18:37 +0800

    Added liquibase files

--- airbnb-clone-back/src/main/java/fr/codecake/airbnb_clone_back/user/repository/UserRepository.java
+++ airbnb-clone-back/src/main/java/fr/codecake/airbnb_clone_back/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package fr.codecake.airbnb_clone_back.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
+//package fr.codecake.airbnb_clone_back.user.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//}
