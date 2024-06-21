9953437cc17fc7388bebf6c7a5885d93fa0f2c8c
LohYangXian <e0726433@u.nus.edu>
2024-06-21 15:55:22 +0800

    Added SecurityUtils

---
+++ airbnb-clone-back/src/main/java/fr/codecake/airbnb_clone_back/infrastructure/config/SecurityUtils.java
@@ -0,0 +1,12 @@
+package fr.codecake.airbnb_clone_back.infrastructure.config;
+
+public class SecurityUtils {
+
+    public static final String ROLE_TENANT = "ROLE_TENANT";
+    public static final String ROLE_LANDLORD = "ROLE_LANDLORD";
+
+    public static final String CLAIMS_NAMESPACE = "http://codecake.fr/roles";
+
+    public static User map0auth2AttributesToUser(Map<>)
+
+}
