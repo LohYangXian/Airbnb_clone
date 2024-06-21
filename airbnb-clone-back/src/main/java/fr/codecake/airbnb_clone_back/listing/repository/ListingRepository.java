de317eecfc14255b34db38c81066c4c046adab05
LohYangXian <e0726433@u.nus.edu>
2024-06-21 08:38:37 +0800

    Added Bookings, Listings and Users

---
+++ airbnb-clone-back/src/main/java/fr/codecake/airbnb_clone_back/listing/repository/ListingRepository.java
@@ -0,0 +1,8 @@
+package fr.codecake.airbnb_clone_back.listing.repository;
+
+import fr.codecake.airbnb_clone_back.listing.domain.Listing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListingRepository extends JpaRepository<Listing, Long> {
+
+}
--- airbnb-clone-back/src/main/java/fr/codecake/airbnb_clone_back/user/repository/UserRepository.java
+++ airbnb-clone-back/src/main/java/fr/codecake/airbnb_clone_back/user/repository/UserRepository.java
@@ -1,6 +1,14 @@
-//package fr.codecake.airbnb_clone_back.user.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository<User, Long> {
-//}
+package fr.codecake.airbnb_clone_back.user.repository;
+
+import fr.codecake.airbnb_clone_back.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findOneByEmail(String email);
+
+    Optional<User> findOneByPublicId(UUID publicId);
+}
